c45cb60d46d6b61d6e065927ca7c85aa63b99363
abhishekomp <abhishek2283@hotmail.com>
2023-10-30 23:29:40 +0100

    covered computeIfAbsent method test cases

---
+++ src/test/java/org/example/CollectionsUseCaseTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CollectionsUseCaseTest {
+
+    //computeIfAbsent
+    @Test
+    void test_should_return_the_existing_mapped_value_if_key_is_associated_with_a_value() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("John", 5);
+        Integer value = map.computeIfAbsent("John", String::length);
+        assertEquals(value, 5);
+        // It’s important to know that the mapping function is only called if the mapping isn’t present.
+        //computeIfAbsent() : First, it checks if the key is present in the map. If the key is present, and a non-null value is related to the key, then it returns that value.
+        //As we can see, the key “John” has a non-null mapping present, and it returns the value 5. If our mapping function was used, we’d expect the function to return the length of 4.
+    }
+
+    //computeIfAbsent
+    @Test
+    void test_value_is_computed_when_key_is_not_present_in_the_map(){
+        Map<String, Integer> map = new HashMap<>();
+        Integer value = map.computeIfAbsent("John", String::length);
+        assertEquals(value, 4);
+        assertEquals(map.get("John"), 4);
+    }
+
+    //computeIfAbsent
+    @Test
+    void test_value_is_computed_when_key_is_associated_with_null_in_the_map(){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("John", null);
+        Integer value = map.computeIfAbsent("John", String::length);
+        assertEquals(value, 4);
+        assertEquals(map.get("John"), 4);
+    }
+
+}
