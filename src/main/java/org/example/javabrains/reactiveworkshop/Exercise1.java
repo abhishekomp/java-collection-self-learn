591ff4e4a0958164ccac23a24669ca453f9a4731
Abhishek Omprakash <abhishekomprakash@Abhisheks-MacBook-Air.local>
2025-01-06 21:58:50 +0100

    collection_refresh

---
+++ src/main/java/org/example/javabrains/reactiveworkshop/Exercise1.java
@@ -0,0 +1,59 @@
+package org.example.javabrains.reactiveworkshop;
+
+/**
+ * @author : abhishekomprakash
+ * @since : Mon, 2025-Jan-06
+ * Created with IntelliJ IDEA
+ */
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        // Use StreamSources.intNumbersStream() and StreamSources.userStream()
+
+        // Print all numbers in the intNumbersStream stream
+        // TODO: Write code here
+        StreamSource.intNumbersStream().forEach(System.out::println);
+        System.out.println("************************");
+
+        // Print numbers from intNumbersStream that are less than 5
+        // TODO: Write code here
+        StreamSource.intNumbersStream()
+                .filter(num -> num < 5)
+                .forEach(System.out::println);
+        System.out.println("************************");
+
+        // Print the second and third numbers in intNumbersStream that's greater than 5
+        // TODO: Write code here
+        StreamSource.intNumbersStream()
+                .filter(num -> num > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+        System.out.println("************************");
+
+        //  Print the first number in intNumbersStream that's greater than 5.
+        //  If nothing is found, print -1
+        // TODO: Write code here
+        System.out.println(
+            StreamSource.intNumbersStream()
+                .filter(num -> num > 5)
+                .findFirst()
+                .orElse(-1)
+        );
+        System.out.println("************************");
+
+        // Print first names of all users in userStream
+        // TODO: Write code here
+        StreamSource.userStream()
+                .map(User::getFirstName)
+                .forEach(System.out::println);
+        System.out.println("************************");
+
+
+        // Print first names in userStream for users that have IDs from number stream
+        // TODO: Write code here
+    }
+
+}
