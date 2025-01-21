c049e55d3010f293e27de345d16e11de11216dee
abhishekomp <abhishek2283@hotmail.com>
2025-01-21 10:29:31 +0100

    Function Behaviour Parameterization

--- src/main/java/org/example/in28Minutes/FP03BehaviourParameterization.java
+++ src/main/java/org/example/in28Minutes/FP03BehaviourParameterization.java
@@ -17,11 +17,14 @@ public class FP03BehaviourParameterization {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 
         Function<Integer, Integer> squareFunction = x -> x * x;
+        Function<Integer, Integer> cubeFunction = x -> x * x * x;
 
         List<Integer> squaredNumbers = mapAndCollect(numbers, squareFunction);
         squaredNumbers.forEach(System.out::println);
     }
 
+    // The takeaway from this demo is that we are passing function behaviour to this method. We can pass another behaviour for example to perform cube.
+    // Take a look at the squareFunction and the cubeFunction that can be passed to the mapAndCollect method. So a user of this method can send an instance of Function Interface.
     private static List<Integer> mapAndCollect(List<Integer> numbers, Function<Integer, Integer> mapperFunction) {
         return numbers.stream()
                 .map(mapperFunction)
