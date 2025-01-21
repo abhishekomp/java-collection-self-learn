816dd7b944569116beb81544191583d89e7cab74
abhishekomp <abhishek2283@hotmail.com>
2025-01-21 11:40:19 +0100

    BiPredicate, BiFunction, BiConsumer Demo

---
+++ src/main/java/org/example/in28Minutes/funcInterfaces01/BiPredicateBiFunctionBiConsumer.java
@@ -0,0 +1,34 @@
+package org.example.in28Minutes.funcInterfaces01;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+
+public class BiPredicateBiFunctionBiConsumer {
+
+    public static void main(String[] args) {
+
+        //BiPredicate Example
+        BiPredicate<Integer, String> biPredicate = (integer, str) -> true;
+
+        BiPredicate<Integer, String> biPredicate2 = (integer, str) -> {
+            return integer < 10 && str.length() > 5;
+        };
+
+        System.out.println(biPredicate.test(10, "javaFunctionalInterfaces"));
+        System.out.println(biPredicate2.test(8, "javaFunctionalInterfaces"));
+        System.out.println(biPredicate2.test(8, "java"));
+
+        //BiFunction Example
+        BiFunction<Integer, String, String> biFunction = (number, str) -> {
+            return number + " " + str;
+        };
+        System.out.println(biFunction.apply(20, "years ahead"));
+
+        //BiConsumer Example
+        BiConsumer<Integer, String> biConsumer = (number, str) -> {
+            System.out.println(number + " " + str);
+        };
+        biConsumer.accept(25, "years ahead");
+    }
+}
