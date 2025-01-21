ec4d972bfd68629bc52c2ef2ae5f67c30efb6236
abhishekomp <abhishek2283@hotmail.com>
2025-01-21 11:49:08 +0100

    Supplier Functional Interface demo

--- src/main/java/org/example/in28Minutes/funcInterfaces01/BiPredicateBiFunctionBiConsumer.java
+++ src/main/java/org/example/in28Minutes/funcInterfaces01/BiPredicateBiFunctionBiConsumer.java
@@ -3,6 +3,7 @@ package org.example.in28Minutes.funcInterfaces01;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
+import java.util.function.Supplier;
 
 public class BiPredicateBiFunctionBiConsumer {
 
@@ -30,5 +31,10 @@ public class BiPredicateBiFunctionBiConsumer {
             System.out.println(number + " " + str);
         };
         biConsumer.accept(25, "years ahead");
+
+        // Supplier Functional interface example.
+        Supplier<String> supplier = () -> {
+            return "functional interface";
+        };
     }
 }
