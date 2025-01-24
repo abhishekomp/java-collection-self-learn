891fd9e4173162b7caceb188367208c22728828c
abhishekomp <abhishek2283@hotmail.com>
2025-01-24 09:45:42 +0100

    creating a comparator by passing a function to the Comparator.comparing() method

--- src/main/java/org/example/in28Minutes/stream_customclass/FP04ComparatorSorting.java
+++ src/main/java/org/example/in28Minutes/stream_customclass/FP04ComparatorSorting.java
@@ -2,6 +2,7 @@ package org.example.in28Minutes.stream_customclass;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
 
@@ -18,6 +19,27 @@ public class FP04ComparatorSorting {
                 .sorted(Comparator.comparingInt(String::length).thenComparing(Comparator.reverseOrder()))
                 .collect(toList()));
 
+        // Creating a comparator and then using it in the sorted method for sorting a List.
+        // Note: Comparator.comparing() takes a function that is essentially a key extractor, here we are creating a function that will be the argument to the comparing method.
+        Function<String, Integer> strLengthFunc = (str) -> {
+            return str.length();
+        };
+        Comparator<String> stringComparator = Comparator.comparing(strLengthFunc);
 
+        // Sort using comparing method
+        Comparator<String> stringComparator1 = Comparator.comparing((String str) -> {
+            return str.length();
+        });
+
+        System.out.println("Courses before comparing using stringComparator: \n" + courses);
+        List<String> sortedCourses = courses.stream()
+                .sorted(stringComparator)
+                .collect(toList());
+        System.out.println("Courses after comparing using stringComparator: \n" + sortedCourses);
+
+//        List<String> sortedCourses2 = courses.stream()
+//                .sorted(Comparator.comparing(String::length))
+//                .collect(toList());
+//        System.out.println("Courses after comparing using stringComparator: \n" + courses);
     }
 }
