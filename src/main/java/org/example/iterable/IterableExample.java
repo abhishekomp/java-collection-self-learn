4d357ba2da7656a6327d579c29e27c0bca2a8bea
abhishekomp <abhishek2283@hotmail.com>
2025-05-16 17:18:33 +0200

    added more code for Iterable example

--- src/main/java/org/example/iterable/IterableExample.java
+++ src/main/java/org/example/iterable/IterableExample.java
@@ -11,8 +11,9 @@ public class IterableExample {
 
         // Difference between Iterable and Iterator is that an Iterable is a source of Iterator
         // From an Iterable, it is possible to obtain an Iterator
+        // An Iterable is a source of elements, while an Iterator is a cursor that allows you to traverse the elements.
         Collection<String> collection = list;
-        Iterable<String> iterable = collection;
+        Iterable<String> iterable = list;
 
         // Ways to iterate over an iterable
         // Method 1
@@ -30,5 +31,18 @@ public class IterableExample {
         iterable.forEach(item -> System.out.println(item));
 
         Spliterator<String> spliterator = iterable.spliterator();
+
+        // Method 4
+        // Spliterator is a special type of iterator that can be used to traverse elements in parallel
+        // It is used in the Java Stream API to split a collection into smaller parts for parallel processing
+        // Spliterator<String> spliterator = iterable.spliterator();
+        // spliterator.forEachRemaining(System.out::println);
+        // Method 5
+        // Stream<String> stream = iterable.stream();
+        // stream.forEach(System.out::println);
+        // Method 6
+        // Stream<String> stream = iterable.parallelStream();
+        // stream.forEach(System.out::println);
+
     }
 }
